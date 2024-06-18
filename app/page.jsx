de46c677231fe697d454806d372389c42746131e
Sabarish <sabarishharidas@gmail.com>
2024-06-18 16:09:17 +0530

    mixpanel

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
+import { useEffect } from "react";
 
+import Mixpanel from "./utils/mixpanel";
 import StudentForm from "./pages/studentform/page";
+import { useRouter } from "next/navigation";
+
 function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      Mixpanel?.track("Page View", { path: url });
+    };
+
+    // Track initial page load
+    Mixpanel?.track("Page View", { path: router.asPath });
+
+    // Add router event listener for route changes
+    router?.events?.on("routeChangeComplete", handleRouteChange);
+
+    // Clean up event listener
+    return () => {
+      router?.events?.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <main>
       <StudentForm />
---
+++ app/utils/mixpanel.js
@@ -0,0 +1,7 @@
+// utils/mixpanel.js
+
+import Mixpanel from "mixpanel-browser";
+
+Mixpanel.init("9ccef57608668c7329955e61c8434bdb");
+
+export default Mixpanel;
